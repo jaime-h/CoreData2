8139adbf51f5730462dc6227b767d1deb410a1dd
jaime-h <jaime.e.hernandez@gmail.com>
2014-12-17 18:24:34 -0600

    Wire storyboard

---
+++ CoreData2/ViewController.h
@@ -0,0 +1,24 @@
+//
+//  ViewController.h
+//  CoreData2
+//
+//  Created by Jaime Hernandez on 12/17/14.
+//  Copyright (c) 2014 Jaime Hernandez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UITextField *name;
+@property (strong, nonatomic) IBOutlet UITextField *address;
+@property (strong, nonatomic) IBOutlet UITextField *phone;
+@property (strong, nonatomic) IBOutlet UILabel     *status;
+
+- (IBAction)saveData:(id)sender;
+- (IBAction)findContact:(id)sender;
+
+
+
+@end
+
